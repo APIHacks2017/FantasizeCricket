d133c0579ce3e8564026f688bd76865a5759621b
Senthil-Krish <catchsenthil.k@gmail.com>
2017-06-17 19:28:47 +0530

    Updated for player details

--- www/Initialize.js
+++ www/Initialize.js
@@ -41,12 +41,24 @@ function loadMatches() {
 						newRow.onclick = function(){
 							var cells = this.getElementsByTagName("td");
 							matchId = cells[4].innerText;
-							window.location.href = "chooseplayers.html";
+							if(cells[5].innerText == "true")
+							{
+								loadPlayers(matchId);
+							}
+							else
+							{
+								alert("Squad value must be true to Join");
+							}
 						}
 						
 						newCell = document.createElement("td");
 						newCell.style = "display:none;";
 						newCell.textContent = matches[i]["unique_id"];
+						newRow.appendChild(newCell);
+						
+						newCell = document.createElement("td");
+						//newCell.style = "display:none;";
+						newCell.textContent = matches[i]["squad"];
 						newRow.appendChild(newCell);
 						
 						
@@ -66,10 +78,11 @@ function getMatchUniqueId(element){
 	
 }
 
-function loadPlayers() {
+function loadPlayers(mid) {
     var request = new XMLHttpRequest();
+	var choosenMatchId = mid;
 	
-    request.open("GET", "http://52.36.211.72:5555/gateway/Cricket%20API/1.0/fantasySquad?apikey=0CWPTXYxJmaPWm0eJMGwsrlqR9K2&unique_id="+matchId, true);
+    request.open("GET", "http://52.36.211.72:5555/gateway/Cricket%20API/1.0/fantasySquad?apikey=0CWPTXYxJmaPWm0eJMGwsrlqR9K2&unique_id="+choosenMatchId, true);
 	request.setRequestHeader("x-Gateway-APIKey", "f789552a-d69a-4be1-a822-325814165355");
     request.onreadystatechange = function() {//Call a function when the state changes.
         if (request.readyState == 4) {
@@ -80,48 +93,25 @@ function loadPlayers() {
 
 				// Reset the table
 				tableBody.innerHTML = "";
-
+				
 				for (var i=0; i < players.length; i++) {
-					newRow = document.createElement("tr");
-					tableBody.appendChild(newRow);
-
+					for (var j = 0; j < players[i]["players"].length; j++)
+					{
+						newRow = document.createElement("tr");
 						newCell = document.createElement("td");
-						newCell.textContent = matches[i]["date"];
+						newCell.textContent = players[i]["players"][j]["name"];
 						newRow.appendChild(newCell);
-
-						newCell = document.createElement("td");
-						newCell.textContent = matches[i]["team-1"] + " vs " + matches[i]["team-2"];
-						newRow.appendChild(newCell);
-
-						newCell = document.createElement("td");
-						newCell.textContent = matches[i]["matchStarted"];
-						newRow.appendChild(newCell);
+						tableBody.appendChild(newRow);
 						
 						newCell = document.createElement("td");
 						var link = document.createElement("BUTTON");
-						link.innerText = "Join";
-						link.addEventListener("click", getMatchUniqueId, this);
+						link.innerText = "Pick";
 						newCell.appendChild(link);
 						newRow.appendChild(newCell);
-						
-						newRow.onclick = function(){
-							var cells = this.getElementsByTagName("td");
-							matchId = cells[4].innerText;
-							window.location.href = "chooseplayers.html";
-						}
-						
-						newCell = document.createElement("td");
-						newCell.style = "display:none;";
-						newCell.textContent = matches[i]["unique_id"];
-						newRow.appendChild(newCell);
-						
-						
+					}
 				}
-				
-				console.log(matches);
-            }
+			}
         }
     }
-    console.log("asking for tweets");
     request.send();
 }
